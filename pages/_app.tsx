6da4184a717148a6d738d326e28a1d9b1bdcca7d
TheWarrior09 <the.warrior.0072@gmail.com>
2023-01-20 15:44:31 +0500

    Add required packages, config files and update pages
    Update index, _app and _document pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,46 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from 'src/theme';
+import createEmotionCache from 'src/createEmotionCache';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import Layout from 'src/components/Layout';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+const queryClient = new QueryClient();
+
+interface AppPropsWithLayout extends AppProps {
+  Component: NextPageWithLayout;
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: AppPropsWithLayout) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <QueryClientProvider client={queryClient}>
+          {getLayout(<Component {...pageProps} />)}
+        </QueryClientProvider>
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
