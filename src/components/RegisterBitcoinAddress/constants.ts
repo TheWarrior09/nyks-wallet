b10b4f1f76c89eea55c9725f956adb03a682d18e
TheWarrior09 <the.warrior.0072@gmail.com>
2023-02-06 14:26:07 +0500

    Connect to live chain endpoint from localhost

--- src/components/RegisterBitcoinAddress/constants.ts
+++ src/components/RegisterBitcoinAddress/constants.ts
@@ -1,11 +1,7 @@
 const chainId = 'nyks';
 const coinDenom = 'nyks';
 
-const btcDerivatePath = "m/44'/0'/0'/0/0";
+const twilightRpcUrl = 'https://nyks.twilight-explorer.com/tendermint/';
+const twilightRestUrl = 'https://nyks.twilight-explorer.com/api/';
 
-// const twilightRpcUrl = 'https://nyks.twilight-explorer.com/tendermint/';
-// const twilightRestUrl = 'https://nyks.twilight-explorer.com/api/';
-const twilightRpcUrl = 'http://0.0.0.0:26657/';
-const twilightRestUrl = 'http://0.0.0.0:1317/';
-
-export { chainId, coinDenom, btcDerivatePath, twilightRpcUrl, twilightRestUrl };
+export { chainId, coinDenom, twilightRpcUrl, twilightRestUrl };
