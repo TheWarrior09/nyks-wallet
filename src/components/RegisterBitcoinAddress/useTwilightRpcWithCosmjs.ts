d43265ccd4bb784b852f4e1773e5e035a3e02180
TheWarrior09 <the.warrior.0072@gmail.com>
2023-04-14 15:52:17 +0500

    Create new methods and states to manage btc withdraw

--- src/components/RegisterBitcoinAddress/useTwilightRpcWithCosmjs.ts
+++ src/components/RegisterBitcoinAddress/useTwilightRpcWithCosmjs.ts
@@ -1,10 +1,22 @@
 import { EncodeObject, GeneratedType, OfflineSigner, Registry } from '@cosmjs/proto-signing';
-import { defaultRegistryTypes, isDeliverTxSuccess, SigningStargateClient } from '@cosmjs/stargate';
+import {
+  defaultRegistryTypes,
+  isDeliverTxSuccess,
+  SigningStargateClient,
+  GasPrice,
+  calculateFee,
+  DeliverTxResponse,
+} from '@cosmjs/stargate';
 import { useState } from 'react';
-import { MsgRegisterBtcDepositAddress } from 'src/proto/types/tx';
+import {
+  twilightprojectProtoRegistry,
+  getSigningTwilightprojectClient,
+  twilightproject,
+} from 'src/codegen';
 import { chainId, coinDenom, twilightRpcUrl } from './constants';
+import { MsgWithdrawBtcRequest } from 'src/codegen/nyks/bridge/tx';
+import { useMutation } from '@tanstack/react-query';
 
-//
 const msgRegisterBtcDepositAddressTypeUrl =
   '/twilightproject.nyks.bridge.MsgRegisterBtcDepositAddress';
 
@@ -27,6 +39,19 @@ function createDefaultRegistry(): Registry {
 }
 
 interface IuseKeplrWallet {
+async function getSigningClient() {
+  if (!window.keplr) return;
+  try {
+    const offlineSigner: OfflineSigner = window.keplr.getOfflineSigner!(chainId);
+    const signingClient = await getSigningTwilightprojectClient({
+      rpcEndpoint: twilightRpcUrl,
+      signer: offlineSigner,
+    });
+    return signingClient;
+  } catch (error) {
+    throw error;
+  }
+}
   btcAddress: string | undefined;
   twilightAddress: string | undefined;
 }
@@ -87,10 +112,47 @@ export const useTwilightRpcWithCosmjs = ({ btcAddress, twilightAddress }: IuseKe
     }
   };
 
+  const signAndBroadcastWithdrawBtcTx = async (msg: MsgWithdrawBtcRequest) => {
+    const signingClient = await getSigningClient();
+    if (signingClient === undefined) return;
+    const { withdrawBtcRequest } = twilightproject.nyks.bridge.MessageComposer.withTypeUrl;
+    const msgWithdrawBtcRequest = withdrawBtcRequest({
+      withdrawAddress: msg.withdrawAddress,
+      reserveAddress: msg.reserveAddress,
+      withdrawAmount: msg.withdrawAmount,
+      twilightAddress: msg.twilightAddress,
+    });
+    const gasPrice = GasPrice.fromString('1nyks');
+    const gasEstimation = await signingClient.simulate(
+      msg.twilightAddress,
+      [msgWithdrawBtcRequest],
+      '',
+    );
+    const fee = calculateFee(Math.round(gasEstimation * 1.3), gasPrice);
+    return signingClient.signAndBroadcast(msg.twilightAddress, [msgWithdrawBtcRequest], fee);
+  };
+
+  const {
+    data: msgBtcWithdrawResponseData,
+    error: msgBtcWithdrawResponseError,
+    status: msgBtcWithdrawResponseStatus,
+    mutate: withdrawBtcFromNyks,
+  } = useMutation({
+    mutationFn: signAndBroadcastWithdrawBtcTx,
+  });
+
+  const getTransactionStatus = (txResponse: DeliverTxResponse) =>
+    isDeliverTxSuccess(txResponse) ? 'Success' : 'Failed';
+
   return {
     registerBtcAddressOnNyks,
     txIdNYKS,
     registerBtcAddressOnNyksLoadingState: loading,
     isDepositAddressRegistered,
+    withdrawBtcFromNyks,
+    msgBtcWithdrawResponseData,
+    msgBtcWithdrawResponseError,
+    msgBtcWithdrawResponseStatus,
+    getTransactionStatus,
   };
 };
