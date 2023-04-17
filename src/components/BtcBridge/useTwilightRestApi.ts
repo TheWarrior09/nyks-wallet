6bcce89b3f38308cafeb7faf3e05ef1ae8749988
TheWarrior09 <the.warrior.0072@gmail.com>
2023-04-17 17:01:51 +0500

    Infer all the things and no more manually specifying Generics
    Advantages of this approach are:
    
    no more manually specifying Generics
    works for cases where the 3rd (select) and 4th (QueryKey) Generic are needed
    will continue to work if more Generics are added
    code is less confusing / looks more like JavaScript

--- src/components/BtcBridge/useTwilightRestApi.ts
+++ src/components/BtcBridge/useTwilightRestApi.ts
@@ -7,7 +7,6 @@ import {
 import { twilightRestUrl } from './constants';
 import { useQuery } from '@tanstack/react-query';
 import { queryFunctionWithAxios } from './utils';
-import { AxiosError } from 'axios';
 
 interface UseTwilightRestApi {
   twilightAddress: string | undefined;
@@ -16,12 +15,7 @@ interface UseTwilightRestApi {
 export const useTwilightRestApi = ({ twilightAddress }: UseTwilightRestApi) => {
   const BTC_DEPOSIT_ADDRESS_ENDPOINT = `${twilightRestUrl}twilight-project/nyks/bridge/registered_btc_deposit_address_by_twilight_address/${twilightAddress}`;
 
-  const registeredBtcDepositAddressQuery = useQuery<
-    RegisteredBtcDepositAddress,
-    AxiosError,
-    RegisteredBtcDepositAddress,
-    string[]
-  >({
+  const registeredBtcDepositAddressQuery = useQuery<RegisteredBtcDepositAddress>({
     queryKey: ['registered_btc_deposit_address_by_twilight_address', BTC_DEPOSIT_ADDRESS_ENDPOINT],
     queryFn: queryFunctionWithAxios,
     enabled: !!twilightAddress,
@@ -32,12 +26,7 @@ export const useTwilightRestApi = ({ twilightAddress }: UseTwilightRestApi) => {
 
   const RESERVE_SCRIPT_ADDRESS_ENDPOINT = `${twilightRestUrl}twilight-project/nyks/bridge/registered_reserve_scripts`;
 
-  const registeredReserveScriptsQuery = useQuery<
-    RegisteredReserveScriptsResponse,
-    AxiosError,
-    RegisteredReserveScriptsResponse,
-    string[]
-  >({
+  const registeredReserveScriptsQuery = useQuery<RegisteredReserveScriptsResponse>({
     queryKey: ['registered_reserve_scripts', RESERVE_SCRIPT_ADDRESS_ENDPOINT],
     queryFn: queryFunctionWithAxios,
     refetchOnWindowFocus: false,
@@ -48,17 +37,12 @@ export const useTwilightRestApi = ({ twilightAddress }: UseTwilightRestApi) => {
 
   const ATTESTATIONS_PROPOSAL_TYPE_BTC_DEPOSIT_ENDPOINT = `${twilightRestUrl}twilight-project/nyks/nyks/attestations?proposal_type=PROPOSAL_TYPE_BTC_DEPOSIT`;
 
-  const proposalTypeBtcDepositQuery = useQuery<
-    ProposalTypeBtcDeposit,
-    AxiosError,
-    ProposalTypeBtcDeposit['attestations'],
-    string[]
-  >({
+  const proposalTypeBtcDepositQuery = useQuery({
     queryKey: ['attestations', ATTESTATIONS_PROPOSAL_TYPE_BTC_DEPOSIT_ENDPOINT],
     queryFn: queryFunctionWithAxios,
     enabled: !!twilightAddress,
     refetchInterval: 5000,
-    select: (data) =>
+    select: (data: ProposalTypeBtcDeposit) =>
       data.attestations.length > 0
         ? data.attestations.filter(
             (attestation) => attestation.proposal.twilightDepositAddress === twilightAddress,
@@ -68,17 +52,12 @@ export const useTwilightRestApi = ({ twilightAddress }: UseTwilightRestApi) => {
 
   const ATTESTATIONS_PROPOSAL_TYPE_BTC_WITHDRAW_ENDPOINT = `${twilightRestUrl}twilight-project/nyks/nyks/attestations?proposal_type=PROPOSAL_TYPE_BTC_WITHDRAW`;
 
-  const proposalTypeBtcWithdrawQuery = useQuery<
-    ProposalTypeBtcWithdraw,
-    AxiosError,
-    ProposalTypeBtcWithdraw['attestations'],
-    string[]
-  >({
+  const proposalTypeBtcWithdrawQuery = useQuery({
     queryKey: ['attestations', ATTESTATIONS_PROPOSAL_TYPE_BTC_WITHDRAW_ENDPOINT],
     queryFn: queryFunctionWithAxios,
     enabled: !!twilightAddress,
     refetchInterval: 5000,
-    select: (data) =>
+    select: (data: ProposalTypeBtcWithdraw) =>
       data.attestations.length > 0
         ? data.attestations.filter(
             (attestation) => attestation.proposal.twilightDepositAddress === twilightAddress,
